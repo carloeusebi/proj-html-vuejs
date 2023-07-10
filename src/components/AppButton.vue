eb4030267bc36ab0e9f9b0c7b13000569e22dbf5
carloeusebi <carloeusebi@gmail.com>
2023-07-10 16:53:51 +0200

    Completed jumbo

---
+++ src/components/AppButton.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+	props: {
+		text: {
+			type: String,
+			required: true,
+		},
+		href: {
+			type: String,
+			default: '#',
+		},
+		type: {
+			type: String,
+			default: 'primary',
+			validator: type => ['primary', 'secondary'].includes(type),
+		},
+	},
+	computed: {},
+};
+</script>
+
+<template>
+	<a
+		:href="href"
+		:class="type"
+		>{{ text }}</a
+	>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/sass/colors' as *;
+a {
+	display: inline-block;
+	padding: 1rem 3rem;
+	font-weight: bold;
+	text-transform: uppercase;
+}
+
+.primary {
+	background-color: $background-blue;
+	color: white;
+}
+
+.secondary {
+	background-color: white;
+	border: 2px solid gray;
+	color: rgb(128, 128, 128);
+}
+</style>
--- src/components/header/HeaderJumbo.vue
+++ src/components/header/HeaderJumbo.vue
@@ -1,23 +1,26 @@
 <script>
-export default {};
+import AppButton from '../AppButton.vue';
+export default { components: { AppButton } };
 </script>
 
 <template>
 	<div
 		id="jumbotron"
 		class="text-center">
-		<h1>TITLE</h1>
+		<h1>Contemporary Ideas</h1>
 		<p>
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente odit illo consequuntur exercitationem dolore,
 			ipsam molestiae labore eveniet cupiditate voluptatibus ullam doloremque cum officiis harum tenetur ipsum
 			repellendus libero. Excepturi!
 		</p>
-		<a
-			href=""
-			class="btn"
-			>Register now</a
-		>
+		<AppButton text="Register now" />
 	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+p {
+	font-size: 1.1rem;
+	line-height: 2;
+	margin-bottom: 3rem;
+}
+</style>

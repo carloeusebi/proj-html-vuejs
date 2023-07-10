413590ee105e51f8b7c5048d144964fad658a81f
carloeusebi <carloeusebi@gmail.com>
2023-07-10 16:21:15 +0200

    Installed fontawesome

--- src/main.js
+++ src/main.js
@@ -3,5 +3,10 @@ import App from './App.vue';
 import '@fontsource/merriweather';
 import '@/assets/sass/index.scss';
 import * as bootstrap from 'bootstrap';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faMagnifyingGlass, faBars, faBagShopping } from '@fortawesome/free-solid-svg-icons';
 
-createApp(App).mount('#app');
+library.add(faBagShopping, faMagnifyingGlass, faBars);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');

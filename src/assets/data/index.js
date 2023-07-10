42808eabe619ead2d9d004c481858aaa4146ee53
carloeusebi <carloeusebi@gmail.com>
2023-07-10 23:45:10 +0200

    Completed left column of  learning section

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,9 +1,9 @@
 import { boxes } from './_boxes';
 import { courses } from './_courses';
-import { list as learningList } from './_learning';
+import { list as learningList, descriptions } from './_learning';
 import { navbarLinks } from './_navbar';
 import { plans, perks } from './_pricing';
 import { quotes } from './_quotes';
 import { jumbo } from './_jumbo';
 
-export { boxes, courses, learningList, navbarLinks, plans, perks, quotes, jumbo };
+export { boxes, courses, learningList, navbarLinks, plans, perks, quotes, jumbo, descriptions };
--- src/components/main/MainLearning.vue
+++ src/components/main/MainLearning.vue
@@ -1,9 +1,76 @@
 <script>
-export default {};
+import { learningList, descriptions } from '@/assets/data';
+
+export default {
+	data() {
+		return {
+			learningList,
+			descriptions,
+			active: learningList[0].id,
+		};
+	},
+};
 </script>
 
 <template>
-	<h2>Learning Possibilities</h2>
+	<section id="learning-possibilities">
+		<div class="container">
+			<div class="grid">
+				<div class="left-column">
+					<ul>
+						<li
+							v-for="item in learningList"
+							@click="active = item.id"
+							:key="item.id"
+							:class="{ active: item.id === active }"
+							class="cell">
+							{{ item.text }}
+						</li>
+					</ul>
+				</div>
+				<div class="right-column">
+					<h2>Learning Possibilities</h2>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use '@/assets/sass/colors' as *;
+@use '@/assets/sass' as *;
+
+section {
+	margin: 8rem 0;
+}
+.grid {
+	display: grid;
+	grid-template-columns: 30% 70%;
+	gap: 2rem;
+}
+
+* {
+	color: $text-light-gray;
+}
+
+h2 {
+	color: black;
+}
+
+li {
+	font-family: $font-primary;
+	font-weight: 600;
+	padding: 1.5rem;
+	border: 1px solid #eee;
+	user-select: none;
+	cursor: pointer;
+	transition: all 250ms ease-out;
+
+	&.active,
+	&:hover {
+		background-color: #fcfcfc;
+		color: $background-blue;
+		border-left: 7px solid $background-blue;
+	}
+}
+</style>

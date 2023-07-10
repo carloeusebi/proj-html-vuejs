80d39f30f73fef952e25d69cb21c68659eff8ce1
carloeusebi <carloeusebi@gmail.com>
2023-07-10 16:07:15 +0200

    Indexed data folder and fixed config files

---
+++ src/assets/data/index.js
@@ -0,0 +1,7 @@
+import { boxes } from './_boxes';
+import { courses } from './_courses';
+import { list as learningList } from './_learning';
+import { navbarLinks } from './_navbar';
+import { plans, perks } from './_pricing';
+
+export default { boxes, courses, learningList, navbarLinks, plans, perks };

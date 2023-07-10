f97b8ab0205016042098b60e04fd3ba2ae451c10
carloeusebi <carloeusebi@gmail.com>
2023-07-10 18:17:28 +0200

    Completed potential section

--- src/components/main/MainContentPotential.vue
+++ src/components/main/MainContentPotential.vue
@@ -3,7 +3,62 @@ export default {};
 </script>
 
 <template>
-	<h2>Empowering children to reach their full potential</h2>
+	<div class="container">
+		<section id="potential">
+			<div class="grid">
+				<div class="left-column">
+					<img
+						src="@/assets/img/h5-img-2.jpg"
+						alt="People holding a globe" />
+				</div>
+				<div class="right-column">
+					<h2>Empowering children to reach their full potential</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio dolor itaque nemo eligendi saepe unde incidunt
+						adipisci porro assumenda quasi voluptas, labore modi et. Nihil labore ratione eius deserunt consectetur.
+					</p>
+					<div class="grid">
+						<div class="left-column blue-text">
+							<div class="number">168</div>
+							<div class="description">User Stories</div>
+						</div>
+						<div class="right-column blue-text">
+							<div class="number">347</div>
+							<div class="description">Events</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@use '@/assets/sass/index' as *;
+
+#potential {
+	margin-top: 8rem;
+}
+
+img {
+	padding: 0 2rem;
+}
+.grid {
+	display: grid;
+	grid-template-columns: 60% 40%;
+}
+
+.number,
+.description {
+	font-family: $font-primary;
+	font-weight: 500;
+}
+
+.number {
+	font-size: 4rem;
+}
+
+.description {
+	font-size: 1.1rem;
+}
+</style>

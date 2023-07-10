e5615ffe3bdc390e4edb67fcb3e550ab3ba3743f
carloeusebi <carloeusebi@gmail.com>
2023-07-10 23:27:48 +0200

    Enriched learning data

--- src/assets/data/_learning.js
+++ src/assets/data/_learning.js
@@ -1,8 +1,89 @@
 export const list = [
-	'What We Do',
-	'Degree Programme',
-	'Career Achievements',
-	'Personal Management',
-	'Steps To Success',
-	'Knowledge Transfer',
+	{ id: 1, text: 'What We Do' },
+	{ id: 2, text: 'Degree Programme' },
+	{ id: 3, text: 'Career Achievements' },
+	{ id: 4, text: 'Personal Management' },
+	{ id: 5, text: 'Steps To Success' },
+	{ id: 6, text: 'Knowledge Transfer' },
+];
+
+export const descriptions = [
+	{
+		id: 1,
+		title: 'Learning Possibilities',
+		paragraph:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat amet exercitationem ullam dolore. Esse non, quibusdam iusto perferendis rem nam quae sint itaque architecto praesentium. Possimus cupiditate fuga maiores maxime.',
+		bulletPoints: [
+			'We enrich lives through learning',
+			'Maximizing potential through individual attention',
+			'The trusted name for specialized training',
+			'People teach. People learn. This is where tey connect',
+		],
+		img: 'h12-tabs-icon-1.png',
+	},
+	{
+		id: 2,
+		title: 'Inspiring Minds',
+		paragraph:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat amet exercitationem ullam dolore. Esse non, quibusdam iusto perferendis rem nam quae sint itaque architecto praesentium. Possimus cupiditate fuga maiores maxime.',
+		bulletPoints: [
+			'We enrich lives through learning',
+			'Maximizing potential through individual attention',
+			'The trusted name for specialized training',
+			'People teach. People learn. This is where tey connect',
+		],
+		img: 'h12-tabs-icon-2.png',
+	},
+	{
+		id: 3,
+		title: 'Investing in Knowledge',
+		paragraph:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat amet exercitationem ullam dolore. Esse non, quibusdam iusto perferendis rem nam quae sint itaque architecto praesentium. Possimus cupiditate fuga maiores maxime.',
+		bulletPoints: [
+			'We enrich lives through learning',
+			'Maximizing potential through individual attention',
+			'The trusted name for specialized training',
+			'People teach. People learn. This is where tey connect',
+		],
+		img: 'h12-tabs-icon-3.png',
+	},
+	{
+		id: 4,
+		title: 'Secure your future',
+		paragraph:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat amet exercitationem ullam dolore. Esse non, quibusdam iusto perferendis rem nam quae sint itaque architecto praesentium. Possimus cupiditate fuga maiores maxime.',
+		bulletPoints: [
+			'We enrich lives through learning',
+			'Maximizing potential through individual attention',
+			'The trusted name for specialized training',
+			'People teach. People learn. This is where tey connect',
+		],
+		img: 'h12-tabs-icon-4.png',
+	},
+	{
+		id: 5,
+		title: 'Where Learning Begins',
+		paragraph:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat amet exercitationem ullam dolore. Esse non, quibusdam iusto perferendis rem nam quae sint itaque architecto praesentium. Possimus cupiditate fuga maiores maxime.',
+		bulletPoints: [
+			'We enrich lives through learning',
+			'Maximizing potential through individual attention',
+			'The trusted name for specialized training',
+			'People teach. People learn. This is where tey connect',
+		],
+		img: 'h12-tabs-icon-5.png',
+	},
+	{
+		id: 6,
+		title: 'Knowledge is Power',
+		paragraph:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat amet exercitationem ullam dolore. Esse non, quibusdam iusto perferendis rem nam quae sint itaque architecto praesentium. Possimus cupiditate fuga maiores maxime.',
+		bulletPoints: [
+			'We enrich lives through learning',
+			'Maximizing potential through individual attention',
+			'The trusted name for specialized training',
+			'People teach. People learn. This is where tey connect',
+		],
+		img: 'h12-tabs-icon-6.png',
+	},
 ];

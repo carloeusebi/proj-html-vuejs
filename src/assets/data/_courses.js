e2c533c8101ca2a007bb6e095d811727237191db
carloeusebi <carloeusebi@gmail.com>
2023-07-11 09:49:38 +0200

    Enriched courses data

--- src/assets/data/_courses.js
+++ src/assets/data/_courses.js
@@ -1,5 +1,32 @@
 export const courses = [
 	{
+		img: 'course-1-f-img.jpg',
+		title: 'Business English',
+		pricing: 20,
+		author: 'Preston Marshall',
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat amet exercitationem ullam dolore. Esse non, quibusdam iusto perferendis rem nam quae sint itaque architecto praesentium. Possimus cupiditate fuga maiores maxime.',
+		type: 'Languages',
+	},
+	{
+		img: 'course-2-f-img.jpg',
+		title: 'Social Computing',
+		pricing: 0,
+		author: 'David Sanders',
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat amet exercitationem ullam dolore. Esse non, quibusdam iusto perferendis rem nam quae sint itaque architecto praesentium. Possimus cupiditate fuga maiores maxime.',
+		type: 'Programming',
+	},
+	{
+		img: 'course-2-f-img.jpg',
+		title: 'Learn Spanish',
+		pricing: 20,
+		author: 'Jennie King',
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat amet exercitationem ullam dolore. Esse non, quibusdam iusto perferendis rem nam quae sint itaque architecto praesentium. Possimus cupiditate fuga maiores maxime.',
+		type: 'Languages',
+	},
+	{
 		img: 'course-5-f-img.jpg',
 		title: 'Android Developer',
 		pricing: 0,
@@ -22,6 +49,33 @@ export const courses = [
 		title: 'Financial Modeling',
 		pricing: 0,
 		author: 'Edward Bowman',
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat amet exercitationem ullam dolore. Esse non, quibusdam iusto perferendis rem nam quae sint itaque architecto praesentium. Possimus cupiditate fuga maiores maxime.',
+		type: 'Business',
+	},
+	{
+		img: 'course-4-f-img.jpg',
+		title: 'Basic Marketing',
+		pricing: 0,
+		author: 'Edward Bowman',
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat amet exercitationem ullam dolore. Esse non, quibusdam iusto perferendis rem nam quae sint itaque architecto praesentium. Possimus cupiditate fuga maiores maxime.',
+		type: 'Business',
+	},
+	{
+		img: 'course-11-f-img.jpg',
+		title: 'Academic English',
+		pricing: 0,
+		author: 'Dave Robbins',
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat amet exercitationem ullam dolore. Esse non, quibusdam iusto perferendis rem nam quae sint itaque architecto praesentium. Possimus cupiditate fuga maiores maxime.',
+		type: 'Languages',
+	},
+	{
+		img: 'course-2-f-img.jpg.jpg',
+		title: 'Modern Psychology',
+		pricing: 40,
+		author: 'Kathryn Webb',
 		description:
 			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat amet exercitationem ullam dolore. Esse non, quibusdam iusto perferendis rem nam quae sint itaque architecto praesentium. Possimus cupiditate fuga maiores maxime.',
 		type: 'Business',

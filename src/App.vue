be45ba6925b2d789be91d4bb8504177a2403b1a4
carloeusebi <carloeusebi@gmail.com>
2023-07-11 17:13:40 +0200

    Added side buttons

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import AppSideButton from './components/AppSideButton.vue';
 
 export default {
-	components: { AppHeader, AppMain, AppFooter },
+	components: { AppHeader, AppMain, AppFooter, AppSideButton },
 };
 </script>
 
@@ -12,4 +13,18 @@ export default {
 	<AppHeader />
 	<AppMain />
 	<AppFooter />
+
+	<!-- related button -->
+	<AppSideButton
+		background-color="crimson"
+		img="svg/svg-0.svg"
+		:bottom="250"
+		text="related" />
+
+	<!-- shop button -->
+	<AppSideButton
+		background-color="white"
+		img="svg/svg-2.svg"
+		:bottom="190"
+		text="buy now" />
 </template>
---
+++ src/components/AppSideButton.vue
@@ -0,0 +1,84 @@
+<script>
+export default {
+	props: {
+		backgroundColor: String,
+		img: String,
+		text: String,
+		link: {
+			type: String,
+			default: '#',
+		},
+		bottom: Number,
+	},
+	computed: {
+		imgSrc() {
+			return new URL(`../assets/img/${this.img}`, import.meta.url).href;
+		},
+	},
+};
+</script>
+
+<template>
+	<a :href="link">
+		<div
+			id="related"
+			:class="`bg-${backgroundColor}`"
+			:style="`bottom: ${bottom}px`">
+			<figure>
+				<img
+					:src="imgSrc"
+					:alt="text" />
+			</figure>
+			<span class="text-uppercase">{{ text }}</span>
+		</div>
+	</a>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/sass/colors' as *;
+
+#related {
+	position: fixed;
+	right: -100px;
+	width: 150px;
+	height: 50px;
+	padding: 0.5rem;
+
+	display: flex;
+	align-items: center;
+	cursor: pointer;
+
+	box-shadow: 0 0 9.8px 0.2px rgba(0, 0, 0, 0.1);
+
+	transition: right 500ms;
+
+	figure {
+		width: 50px;
+		height: 100%;
+		margin-bottom: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	img {
+		user-select: none;
+		max-height: 20px;
+		margin-right: 1rem;
+	}
+
+	&:hover {
+		right: 0;
+	}
+}
+
+.bg-white {
+	background-color: white;
+	color: black;
+}
+
+.bg-crimson {
+	background-color: $background-crimson;
+	color: white;
+}
+</style>
